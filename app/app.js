4bc5e117254800838968d25ced87676a6cea52b7
mortl <martin.benes66@gmail.com>
2016-07-18 14:50:47 -0400

    Added Orders component to display specific order per Customer Id.

--- app/app.js
+++ app/app.js
@@ -1,5 +1,18 @@
 	(function() {
 	 
-	 var app = angular.module('customerApp',[]);		 
+	 var app = angular.module('customerApp',['angular.filter','ngRoute']);		 
+
+	 app.config(function($routeProvider){
+	 		$routeProvider
+	 			.when('/', {
+	 				controller:'CustomerController',
+	 				templateUrl:'app/views/customers.html'
+	 			})
+	 			.when('/orders/:customerId', {
+	 				controller:'OrdersController',
+	 				templateUrl:'app/views/orders.html'
+	 			})
+	 			.otherwise( { redirectTo:'/' } );
+	 });
 
 	}());
